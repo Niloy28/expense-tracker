96c4107cbb1f173e0b8bcddcdb0e06b112a78aa1
Farhan Ishrak Islam <niloy28@live.com>
2023-02-15 00:03:29 +0600

    Added ExpenseFilter UI.

---
+++ src/components/ExpenseFilter.tsx
@@ -0,0 +1,20 @@
+import "../styles/ExpenseFilter.css";
+
+const ExpenseFilter = () => {
+	return (
+		<div className="expense-filter">
+			<div>Filter By Year</div>
+			<div>
+				<select className="filter-select" name="years" id="year-select">
+					<option value="2020">2020</option>
+					<option value="2021">2021</option>
+					<option value="2022">2022</option>
+					<option value="2023">2023</option>
+					<option value="2024">2024</option>
+				</select>
+			</div>
+		</div>
+	);
+};
+
+export default ExpenseFilter;
--- src/components/Expenses.tsx
+++ src/components/Expenses.tsx
@@ -3,10 +3,12 @@ import Expense from "../types/ExpensesProps";
 import Card from "./Card";
 
 import "../styles/Expenses.css";
+import ExpenseFilter from "./ExpenseFilter";
 
 const Expenses: React.FC<{ expenses: Expense[] }> = (props) => {
 	return (
 		<Card className="expenses-wrapper">
+			<ExpenseFilter />
 			{props.expenses.map((expense) => (
 				<ExpenseItem
 					key={expense.id}
